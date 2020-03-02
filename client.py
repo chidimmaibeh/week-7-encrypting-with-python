49e085d5c4c425414002a9990ee8890eb8f24e5e
Chidimma <chidimmai@gmail.com>
2020-03-01 18:56:48 -0600

    Updating and Adding Files

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import socket
 
-class Client():
-    
+from cipher import Cipher
+from CA import CertificateAuthority
+
+
+class Client:
+
     def __init__(self, host, port):
         self.socket = socket.socket()
         self.host = host
@@ -9,44 +13,71 @@ class Client():
 
     def getConnection(self):
         self.socket.connect((self.host, self.port))
-    
+
     def endConnection(self):
+        # Inform server
+        client.sendMessage('shutting_down')
         self.socket.close()
 
     def recieveMessage(self):
-       data = self.socket.recv(1024)
-       return data.decode()
-
+        data = self.socket.recv(1024)
+        return data.decode()
 
     def sendMessage(self, message):
         self.socket.sendall(message.encode())
 
-if __name__ == "__main__":
-    # send "Hello" string to the server to receive "Hi" string
-    # any other data "Goodbye" string is received from the Server 
-    # and ends the connection
 
-    message = input("Input message to send :")
+if __name__ == "__main__":
 
     # Initialize a client object. 
-    client = Client(host=socket.gethostname(), port=9500)
-    
-    print("Host: ", client.host)
-    print("Port: ", client.port)
+    client = Client(host=socket.gethostname(), port=443)
+
+    print("[Client] Host: ", client.host)
+    print("[Client] Port: ", client.port)
 
     # initialize connection to the server
     client.getConnection()
 
-    print("sending Message:", message)
-    
-    #send the encoded message to the server
-    client.sendMessage(message)
-    
-    #receive a response from the server
-    receivedMessage = client.recieveMessage()
+    # Ask for server name
+    client.sendMessage('get_server_name')
 
-    print("received Message :", receivedMessage)
+    # receive a response from the server
+    server_name = client.recieveMessage()
+
+    # Check if it is a valid server from Certificate Authority
+    certificate_authority = CertificateAuthority()
+    public_key = certificate_authority.validate_server(server_name)
+
+    # Check if we got servers public key from certificate authority
+    if public_key:
+        # We got public key of server. Now we will send
+        # a message encrypted with servers public key
+        encryptor = Cipher(public_key)
+        encrypted_data = encryptor.encrypt("session cipher key")
+
+        # Send the encrypted message to server
+        client.sendMessage(encrypted_data.decode())
+
+        # receive acknowledgment from server
+        server_response = client.recieveMessage()
+
+        # Decrypt the server response to get acknowledgment message
+        server_acknowledgment = encryptor.decrypt(server_response).decode()
+        print("[Client] Server Acknowledgment message: " + server_acknowledgment)
+
+        # Check if valid acknowledgment is provided by the server
+        if server_acknowledgment == 'session cipher key acknowledgement':
+            # Now begin transferring of data to server
+            data_message = input("Please enter your message for server: ")
+            client.sendMessage(encryptor.encrypt(data_message).decode())
+            # receive acknowledgment from server
+            server_response = client.recieveMessage()
+
+    else:
+        print("[Client] Server name wasn't verified from Certificate Authority")
+        # Send Goodbye to server
+        client.sendMessage('Good Bye')
+        # receive acknowledgment from server
+        server_response = client.recieveMessage()
 
     client.endConnection()
- 
-
--- server.py
+++ server.py
@@ -1,42 +1,101 @@
-import socket 
+import socket
+
+from cryptography.fernet import Fernet
+from CA import CertificateAuthority
+
+from cipher import Cipher
+
 
 class Server:
 
-    #initialize a Server object
+    # initialize a Server object
     def __init__(self):
+        # to use IPv4(AF_INET) and mode of transmission TCP (SOCK_STREAM)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Get hostname of server
         self.host = socket.gethostname()
-        self.port = 9500
+        self.port = 443
         self.socket.bind((self.host, self.port))
+        # Generate a key for server encryption and decryption
+        self.public_key = Fernet.generate_key()
+        # Get server name
+        self.server_name = self.host + str(self.port)
+        self.trusted_clients = list()
 
-    #sends an encoded message over TCP connnection
+    # sends an encoded message over TCP connnection
     def _sendMessage(self, conn, message):
         conn.send(message.encode())
-    
-    #listening for incoming connections
+
+    # listening for incoming connections
     def listen(self, printMesage=True):
         self.socket.listen(5)
         if printMesage:
-            print('Server is Listening new Connection on Port : ', self.port)
-        self._acceptConnecton() 
+            print('[Server] Server is Listening new Connection on Port : ', self.port)
+        self._acceptConnecton()
 
-    #accepts the TCP connections and keeps it alive
+    def get_client_address(self, addr):
+        return addr[0] + ':' + str(addr[1])
+
+    # accepts the TCP connections and keeps it alive
     def _acceptConnecton(self):
-        print("server to accept the new connection")
+        print("[Server] Server to accept the new connection")
         conn, addr = self.socket.accept()
+        encryptor = Cipher(self.public_key)
         with conn:
             while True:
                 # always listen for incoming messages.
                 data = conn.recv(1024)
-                if data == b'Hello':
-                    self._sendMessage(conn, 'Hi')
-                else:
-                    self._sendMessage(conn, 'Goodbye')
+                # Check if client is asking for server name
+                if data == b'get_server_name':
+                    # Send servers name to client
+                    print("[Server] Sending server name to client")
+                    self._sendMessage(conn, self.server_name)
+                # Check if client is closing its connection with the server
+                elif data == b'shutting_down':
+                    # Remove client from trusted list
+                    print("[Server] Client closing the connection")
+                    self.trusted_clients.remove(self.get_client_address(addr))
+                    # Drop connection with current client and start listening for new ones
                     self.listen(False)
+                else:
+                    # Check if client already has sent valid cipher key and is now transferring data
+                    if self.get_client_address(addr) in self.trusted_clients:
+                        if data:
+                            # Client is transferring data right now
+                            print("[Server] Message from client: " + encryptor.decrypt(data.decode()).decode())
+                            ack_message = "OK"
+                            self._sendMessage(conn, encryptor.encrypt(ack_message).decode())
+                    # Lets see if client sent us valid cipher key phrase
+                    elif encryptor.decrypt(data.decode()) == b'session cipher key':
+                        client_address = self.get_client_address(addr)
+                        # Prepare acknowledgment message for client
+                        ack_message = "session cipher key acknowledgement"
+                        # Mark this client trusted
+                        self.trusted_clients.append(client_address)
+                        print("[Server] Client sent valid session cipher key")
+                        # Send acknowledgment message to client
+                        self._sendMessage(conn, encryptor.encrypt(ack_message).decode())
 
- 
+                    else:
+                        # Client has provided invalid session cipher key
+                        ack_message = "Invalid session cipher key"
+                        print("[Server] Client sent invalid session cipher key")
+                        self._sendMessage(conn, encryptor.encrypt(ack_message).decode())
+                        # Drop connection with current client and start listening for new ones
+                        self.listen(False)
+
+    # This method is used by server to register itself with the Certificate Authority
+    def register_with_ca(self):
+        certificate_authority = CertificateAuthority()
+        certificate_authority.register_server(self.server_name, self.public_key)
+
+
 if __name__ == "__main__":
-    #initialize the Server object and starts listening for connections 
+    # initialize the Server object and starts listening for connections
     server1 = Server()
-    server1.listen()
 
+    # Register the server with Certificate Authority
+    server1.register_with_ca()
+
+    # Now listen for connections
+    server1.listen()
